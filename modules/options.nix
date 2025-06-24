2abd08179be4112d0902243a322f2edbf9f80344
pupbrained <mars@pupbrained.xyz>
2025-06-23 22:58:51 -0400

    whatever that works too i guess

--- modules/options.nix
+++ modules/options.nix
@@ -1,7 +1,41 @@
-illogical-impulse-dotfiles: {lib, ...}:
+illogical-impulse-dotfiles: {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib; let
 in {
   options.illogical-impulse = {
     enable = mkEnableOption "Enable illogical-impulse";
+    hyprland = {
+      monitor = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [",preferred,auto,1"];
+        description = "Monitor preferences for Hyprland";
+      };
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.hyprland;
+        description = "Hyprland package";
+      };
+      xdgPortalPackage = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.xdg-desktop-portal-hyprland;
+        description = "xdg-desktop-portal package for Hyprland";
+      };
+      ozoneWayland.enable = lib.mkEnableOption "Set NIXOS_OZONE_WL=1";
+    };
+
+    dotfiles = {
+      anyrun.enable = mkEnableOption "Enable illogical-impulse anyrun";
+      kitty.enable = mkEnableOption "Enable illogical-impulse kitty";
+      fish.enable = mkEnableOption "Enable illogical-impulse fish";
+      starship.enable = mkEnableOption "Enable illogical-impulse starship";
+    };
   };
+
+  config =
+    mkMerge [
+    ];
 }
